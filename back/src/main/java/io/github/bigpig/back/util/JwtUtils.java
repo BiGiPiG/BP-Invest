c12ef700ccb3cb9a8fadaef6b64a838dcb3c0b62
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-09-26 10:10:18 +0300

    refactor: remove unused method

--- back/src/main/java/io/github/bigpig/back/util/JwtUtils.java
+++ back/src/main/java/io/github/bigpig/back/util/JwtUtils.java
@@ -61,25 +61,4 @@ public class JwtUtils {
                 .getPayload()
                 .getSubject();
     }
-
-    public boolean validateJwtToken(String authToken) {
-        try {
-            Jwts.parser()
-                    .verifyWith(key())
-                    .build()
-                    .parseSignedClaims(authToken);
-            return true;
-        } catch (MalformedJwtException e) {
-            System.err.println("Invalid JWT token: " + e.getMessage());
-        } catch (ExpiredJwtException e) {
-            System.err.println("JWT token is expired: " + e.getMessage());
-        } catch (UnsupportedJwtException e) {
-            System.err.println("JWT token is unsupported: " + e.getMessage());
-        } catch (IllegalArgumentException e) {
-            System.err.println("JWT claims string is empty: " + e.getMessage());
-        } catch (JwtException e) {
-            System.err.println("JWT validation error: " + e.getMessage());
-        }
-        return false;
-    }
 }
