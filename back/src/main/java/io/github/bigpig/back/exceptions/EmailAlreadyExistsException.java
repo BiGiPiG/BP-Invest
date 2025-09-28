4182c8849ad93b5648ee658475beb5df2d3a13c6
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-09-28 14:21:22 +0300

    feat: create exception

---
+++ back/src/main/java/io/github/bigpig/back/exceptions/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package io.github.bigpig.back.exceptions;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String email) {
+        super("User with email " + email + "' already exists");
+    }
+}
